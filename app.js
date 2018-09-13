c96ec231669e607e784835bd07c1032e8906e01a
ermondel <ermondel@gmail.com>
2018-09-13 22:17:24 +0300

    feat: get username in os and write it in the file greetings.txt

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ console.log('Starting app');
 
 const fs = require('fs');
 
-fs.appendFile('greetings.txt', 'Hello world!', err => {
+const os = require('os');
+
+const user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}! `, err => {
     if (err) console.log('Unable to write to file.');
 });
