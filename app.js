7c45754eee92dfe860767d31c812060211b9e95e
ermondel <ermondel@gmail.com>
2018-09-14 22:03:27 +0300

    feat: add notes.js module with functions add and addNote

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const fs = require('fs');
 
 const os = require('os');
 
+const notes = require('./notes');
+
 const user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}! `, err => {
+// test
+const test_addNote = notes.addNote();
+console.log('test addNote:', test_addNote);
+const test_addNum = notes.add(9, -2);
+console.log('test addNum:', test_addNum);
+
+/*
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, err => {
     if (err) console.log('Unable to write to file.');
 });
+*/ 
