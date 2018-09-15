5fbf6f8ccc535c1102dfb7c3b127de364f020187
ermondel <ermondel@gmail.com>
2018-09-15 19:44:59 +0300

    test: Add tests of lodash functions: isString and uniq

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log('Starting app');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes');
+const _ = require('lodash');
 
 const user = os.userInfo();
 
@@ -11,6 +12,10 @@ const test_addNote = notes.addNote();
 console.log('test addNote:', test_addNote);
 const test_addNum = notes.add(9, -2);
 console.log('test addNum:', test_addNum);
+console.log(_.isString(true));
+console.log(_.isString('bar'));
+const test_filteredArray = _.uniq(['bar', 1, 'bar', 1, 2, 3, 4]);
+console.log(test_filteredArray);
 
 /*
 fs.appendFile('greetings.txt', `Hello ${user.username}!`, err => {
