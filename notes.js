bb20e183a02a0c76045fc7425840b086bc1f64c1
ermondel <ermondel@gmail.com>
2018-09-29 22:05:09 +0300

    feat: Add code for read note

--- notes.js
+++ notes.js
@@ -33,7 +33,9 @@ const getAll = () => {
 }
 
 const getNote = (title) => {
-    console.log('Getting note:', title);
+    const notes = fetchNotes();
+    const filteredNotes = notes.filter(note => note.title === title);
+    return filteredNotes[0];
 }
 
 const removeNote = (title) => {
