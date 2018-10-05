84786d53ace65bc7e0464396ae74a6a5891551af
ermondel <ermondel@gmail.com>
2018-10-05 14:30:04 +0300

    test: Practice in debugging (inspect)

--- notes.js
+++ notes.js
@@ -46,6 +46,7 @@ const removeNote = (title) => {
 }
 
 const logNote = (note) => {
+    // debugger;
     console.log('--');
     console.log(`Title: ${note.title}`);
     console.log(`Body: ${note.body}`);
---
+++ playground/debugging.js
@@ -0,0 +1,8 @@
+const person = {
+    name: 'Andrew',
+};
+
+person.age = 25;
+person.name = 'Mike';
+
+console.log(person);
