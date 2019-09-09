258f7f558f871fa966ae112490749b256d5076ea
Ermondel <ermondel@gmail.com>
2019-09-09 22:37:23 +0300

    feat: add package - chalk and test printing in color

--- playground/app.js
+++ playground/app.js
@@ -1,6 +1,17 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 const msg = getNotes();
+
 console.log(msg);
 console.log(validator.isEmail('example@mail.com'));
 console.log(validator.isURL('https://example.com'));
+
+const greenMsg = chalk.green('Success!');
+console.log(greenMsg);
+
+const greenBoldMsg = chalk.green.bold('Success!');
+console.log(greenBoldMsg);
+
+const greenBoldInverseMsg = chalk.green.bold.inverse('Success!');
+console.log(greenBoldInverseMsg);
